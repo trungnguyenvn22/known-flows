a31050085fe1e5e69248e0e959637fc1f6793c70
trungnguyenvn22 <demodecoding@gmail.com>
2023-12-12 11:03:30 +0700

    test1

--- vcoja-frontend/src/App.jsx
+++ vcoja-frontend/src/App.jsx
@@ -4,9 +4,6 @@ import LandingPage from "./pages/LandingPage";
 function App() {
   return (
     <>
-      <h1 className="text-cyan-200 text-3xl font-bold underline">
-        hello day la trung
-      </h1>
       <LandingPage></LandingPage>
     </>
   );
