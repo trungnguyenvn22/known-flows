0486144a6bf07380fdb071f9a1cc652767a2859f
trungnguyenvn22 <demodecoding@gmail.com>
2024-08-12 17:05:59 +0700

    upload project fe

--- KF/src/main/java/com/chunchun21/KF/controller/UserController.java
+++ KF/src/main/java/com/chunchun21/KF/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.chunchun21.KF.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import com.chunchun21.KF.entities.User;
 import com.chunchun21.KF.service.UserService;
 
 @RestController
-@CrossOrigin("http://localhost:8080")
+// @CrossOrigin(origins ="http://localhost:8080")
 public class UserController {
 
     @Autowired
